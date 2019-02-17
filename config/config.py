9b2860e56574856364f055faa108024c1073ebf5
scottkleinman <scott.kleinman@csun.edu>
2019-02-17 15:30:46 -0800

    Modify config

---
+++ config/config.py
@@ -0,0 +1,10 @@
+import os
+
+""" Constants """
+
+'''configurations'''
+MONGO_CLIENT = 'mongodb://localhost:27017'
+
+'''file dir'''
+TEMPLATES_DIR = 'projects/templates'
+WORKSPACE_DIR = 'projects/projects-wms'
